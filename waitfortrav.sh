d2c977baa80c8e2a9db8162657ecf21c4723cb2c
githubfoam <lequestionis@gmail.com>
2020-07-01 14:48:51 +0300

    пройти́ без внима́ния

---
+++ waitfortrav.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
+
+echo "========================================================================================="
+sudo make deploy-libvirt
+sudo make deploy-vagrant
+sudo make deploy-boxes
