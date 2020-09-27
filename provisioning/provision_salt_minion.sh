15092dbfd8cc18a33321856f72d0a9fb40549351
githubfoam <lequestionis@gmail.com>
2020-09-27 21:36:07 +0300

    как`их

--- provisioning/provision_salt_minion.sh
+++ provisioning/provision_salt_minion.sh
@@ -18,15 +18,9 @@ echo "==========================================================================
 echo '192.168.18.88 salt' >> /etc/hosts
 cat /etc/hosts
 
-yum install git salt-master -y
-# chkconfig salt-master on
-# cp -f /vagrant/master /etc/salt/
-# service salt-master start
-# service salt-master status
-
-# yum install salt-minion -y
-# chkconfig salt-minion on
-# echo 'master' >> /etc/salt/minion_id
-# service salt-minion start
-# service salt-minion status
-# crontab -l | fgrep -i -v 'salt-key -A -y' | echo '* * * * * salt-key -A -y' | crontab -
+yum install salt-minion -y
+chkconfig salt-minion on
+echo 'client1' >> /etc/salt/minion_id
+echo 'client2' >> /etc/salt/minion_id
+service salt-minion start
+service salt-minion status
